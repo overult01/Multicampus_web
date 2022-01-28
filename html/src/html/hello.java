d9c1340b8e608167eb048dd1be9c14faad8bc7f9
overult01 <overult01@gmail.com>
2022-01-28 17:51:59 +0900

    HTML_1일

---
+++ html/src/html/hello.java
@@ -0,0 +1,22 @@
+package html;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		Scanner stdIn = new Scanner(System.in);
+		String str = stdIn.nextLine();
+		
+	}
+
+}
